cb7ae02a45c6491b47da196fabbeeb78276cede9
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2016-12-05 20:39:58 +0400

    Split image

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 'use strict'
 
-const app = require('express')();
+const express = require('express')
+const app = express();
 const PORT = process.env.PORT || 3000
 
+const html = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>Merry Christmas Amanda!</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="static/css/styles.css">
+    </head>
+    <body>
+      <img src="static/top-left.jpg" id="top-left" />
+      <img src="static/top-right.jpg" id="top-right" />
+      <img src="static/bottom-left.jpg" id="bottom-left" />
+      <img src="static/bottom-right.jpg" id="top-left" />
+    </body>
+  </html>`
+
+app.use('/static', express.static('static'))
+
 app.get('/', (req, res) => {
-  res.status(200).send("Merry Christmas")
+  res.status(200).send(html)
 });
 
 app.listen(PORT, () => {
