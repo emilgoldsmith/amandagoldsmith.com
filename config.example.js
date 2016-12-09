7265addc3b0f3188db6557b978d2c62c055f7b37
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2016-12-09 16:42:01 +0400

    Fixed gitignoring wrong config file

---
+++ config.example.js
@@ -0,0 +1,5 @@
+module.exports = {
+  full_name: "full name of person to receive present",
+  preferred_name: "preferred name the person wants to be called",
+  card_message: "String to be written on christmas card",
+}
--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  full_name: "Amanda Goldsmith Olesen",
-  preferred_name: "Amanda",
-  card_message: "I am so happy to be your big brother!",
-}
