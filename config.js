aeeda178555275c07033ce8d849edff5b27b5d9b
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2016-12-09 16:36:03 +0400

    added favicon, factored out index.html and added config

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  full_name: "Amanda Goldsmith Olesen",
+  preferred_name: "Amanda",
+  card_message: "I am so happy to be your big brother!",
+}
--- index.js
+++ index.js
@@ -2,31 +2,19 @@
 
 const express = require('express')
 const app = express();
-const PORT = process.env.PORT || 3000
+const path = require('path');
+const config = require('./config.js');
+const fs = require('fs');
+const PORT = process.env.PORT || 3000;
 
-const html = `
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <title>Merry Christmas Amanda!</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="static/css/styles.css">
-    </head>
-    <body id="body">
-      <script src="static/src.js"></script>
-      <img src="static/top-left.jpg" id="top-left" class="absolute top-image left-image" />
-      <img src="static/top-right.jpg" id="top-right" class="absolute top-image right-image" />
-      <img src="static/bottom-left.jpg" id="bottom-left" class="absolute bottom-image left-image" />
-      <img src="static/bottom-right.jpg" id="bottom-right" class="absolute bottom-image right-image" />
-      <button class="absolute unwrap-button" id="unwrap-button" onclick="unwrap()">Click Me!</button>
-      Merry Christmas!
-    </body>
-  </html>`;
+let html = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
+// Insert name in title
+html = html.replace('##PREFERRED_NAME##', config.preferred_name);
 
-app.use('/static', express.static('static'))
+app.use('/static', express.static('static'));
 
 app.get('/', (req, res) => {
-  res.status(200).send(html)
+  res.status(200).send(html);
 });
 
 app.listen(PORT, () => {
